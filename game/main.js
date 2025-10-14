3ad7de9a945b34201f17c4d7fe7c9f6dfafc9d91
mi00217 <whb-070-fsp@e-net.nara.jp>
2025-10-14 07:47:27 +0000

    10_14

--- game/main.js
+++ game/main.js
@@ -1,77 +1,69 @@
-document.getElementById("txt").innerText="これはゲームです";
-const canvas=document.getElementById("gameCanvas");
-const ctx=canvas.getContext("2d");
-const player={
-    x:canvas.width/2-15,
-    y:canvas.height-60,
-    width:30,
-    height:30,
-    color:"yellow",
-    life:3,
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const player = {
+    x: canvas.width / 2 - 15,
+    y: canvas.height - 60,
+    width: 30,
+    height: 30,
+    color: "yellow",
+    life: 3,
 };
-const bullets=[];
-const BULLET_SPEED=-10;
+const bullets = [];
+const BULLET_SPEED = -10;
 
-function tryShoot(){
+function tryShoot() {
     bullets.push({
-        x:player.x,
-        y:player.y,
-        width:10,
-        height:10,
+        x: player.x,
+        y: player.y,
+        width: 10,
+        height: 10,
         vy: BULLET_SPEED,
     })
 }
 
 
-
-
-
-window.addEventListener("keydown",(e) =>{
-    if(e.key==="ArrowLeft"){
-        x -=10;
-    } else if(e.key==="ArrowRight"){
-        x +=10;
-    } else if(e.key==="ArrowUp"){
-        //上キーが押されたときの処理
-        x -=10;
-    } else if(e.key==="ArrowDown"){
-        //下キーが押されたときの処理
-        x +=10;
-    }else if(e.key==="Space"){
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+        if(player.x > 10) {
+            player.x -= 10;
+        }
+    } else if (e.key === "ArrowRight") {
+        if(player.x < canvas.width - player.width - 10){
+            player.x += 10;
+        }
+    } else if (e.code === "Space") {
         tryShoot();
     }
 });
 
-let y =0;
-let z=-150;
-function update(){
-    for(let i=0;i<bullets.length;i++){
-        const bullet=bullets[i];
+function update() {
+    for (let i = 0; i < bullets.length; i++) {
+        const bullet = bullets[i];
         bullet.y += bullet.vy;
-        if(bullet,y<0){
-            bullets.splice(i,1);
+        if (bullet.y < 0) {
+            bullets.splice(i, 1);
         }
     }
 }
-let y=0;
-let z=-150;
-function gameLoop(){
- ctx.fillStyle="black";
- ctx.fillRect(0,0,canvas.width,canvas.height);  
 
- ctx.fillStyle="red";
- ctx.fillRect(300,y,30,30);
-    y +=2;
- ctx.fillStyle="red";
- ctx.fillRect(90,y2,30,30);
-    y2 +=2;   
- ctx.fillStyle="blue";
- ctx.fillRect(x,480,30,30);
-    
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    ctx.fillStyle = "white";
+    for(let i=0; i<bullets.length; i++) {
+        const bullet = bullets[i];
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    }   
+}
+
+function gameLoop() {
     update();
     draw();
     requestAnimationFrame(gameLoop);
 }
 gameLoop();
-    
+
